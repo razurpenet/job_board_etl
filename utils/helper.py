115ee922a88e451a263a7934cfcdb745064fd4f6
razurpenet <rashyd1302@gmail.com>
2024-01-02 02:54:28 +0000

    Created new helper functions to check if bucket exists and created function for setting up transformed jobs bucket

--- utils/helper.py
+++ utils/helper.py
@@ -12,7 +12,18 @@ aws_bucketname = config['AWS_ACCESS']['bucketname']
 aws_transformedbucket = config['AWS_ACCESS']['transformed_bucketname']
 aws_region = config['AWS_ACCESS']['region']
 
+#Create s3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access, aws_secret_access_key=aws_secretkey, region_name=aws_region)
 
+#Check if bucket exists 
+def does_bucket_exists(aws_bucketname):
+    try:
+        s3.head_bucket(Bucket=aws_bucketname)
+        return True
+    except Exception as e:
+        return False
+    
+#Create Raw jobs s3 bucket function
 def create_bucket():    
     client = boto3.client(
         's3',
@@ -27,7 +38,7 @@ def create_bucket():
             'LocationConstraint': aws_region
         }
     )
-
+#Create Transformed-jobs bucket function
 def create_transformed_jobs_bucket():    
     client = boto3.client(
         's3',
@@ -36,7 +47,7 @@ def create_transformed_jobs_bucket():
 
     )
 
-    client.create_transformed_bucket(
+    client.create_bucket(
         Bucket=aws_transformedbucket,
         CreateBucketConfiguration={
             'LocationConstraint': aws_region
